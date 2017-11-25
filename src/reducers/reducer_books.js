d6fd520c4f36718b32de3f91c9e8692e08e84bf6
Lina Zoo <linazooper@gmail.com>
2017-11-25 16:10:45 -0600

    form a connection react and redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Women Who Run With the Wolves' },
+        { title: 'Harry Potter' },
+        { title: 'Eloquent Ruby' },
+        { title: 'Javascript: The Good Parts'}
+    ]
+}
